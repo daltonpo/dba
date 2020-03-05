89de3f3d48f096eb8ec24113d076a8a58a0f6ba1
daltonpo <41164448+daltonpo@users.noreply.github.com>
2020-03-05 10:08:59 -0300

    Update comandos.sql

--- postgresql/comandos.sql
+++ postgresql/comandos.sql
@@ -1,3 +1,6 @@
 --Verificar replicação standby
 
  psql -c "select application_name, client_hostname, backend_start, sent_location, write_location from pg_stat_replication order by client_hostname;"
+ 
+ -- queries com mais tempo de execução
+ psql pje_16 -c "SELECT queryid,substring(query, 1, 50) AS short_query,round(total_time::numeric, 2) AS total_time,calls,round((100 * total_time / sum(total_time::numeric) OVER ())::numeric, 2) AS percentage_cpu FROM pg_stat_statements ORDER BY total_time DESC LIMIT 10;"
